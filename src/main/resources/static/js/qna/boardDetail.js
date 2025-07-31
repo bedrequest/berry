c3ba2424c69174054fbb55e142d496fa9bd92cc0
kidfn <kidfn7241@naver.com>
2025-07-31 16:21:23 +0900

    2025-07-31 -2

--- src/main/resources/static/js/qna/boardDetail.js
+++ src/main/resources/static/js/qna/boardDetail.js
@@ -14,7 +14,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const uploadLabel = document.querySelector('.file-upload label[for="ex_filename"]');
   const previewContainer = document.getElementById('imagePreviewContainer');  // 이미지 미리보기
 
-  // 🟡 [Modify] 버튼 클릭 시
+  const bnoValue = document.querySelector('input[name="bno"]').value;
+
+  // [Modify] 버튼 클릭 시
   modBtn.addEventListener('click', () => {
     // 제목, 내용, 작성자 이메일 readonly 해제
     title.removeAttribute('readonly');
@@ -42,14 +44,42 @@ document.addEventListener('DOMContentLoaded', () => {
     if (uploadInput) {
       uploadInput.disabled = false;
     }
+
+
+//file-x (class) 버튼을 보이게 설정 : style="visibility: hidden => visible로 변환"
+    let fileDelBtn = document.querySelectorAll(".file-x");
+    console.log(fileDelBtn);
+    fileDelBtn.forEach(btn =>{
+        btn.style.visibility = "visible";
+        // file-x 버튼을 클릭하면 비동기로 uuid를 보내서 DB상에서 파일 삭제
+        btn.addEventListener('click',(e)=>{
+            let uuid = btn.dataset.uuid;
+            // 비동기 전송
+            fileRemoveToServer(uuid).then(result =>{
+                if(result == "1"){
+                    alert("파일삭제 성공");
+                    let fileX = document.querySelector('.fileX');
+                    console.log(fileX);
+                    fileX.remove();
+                    btn.remove();
+
+                }
+            })
+        })
+    });
   });
 
-  // 🔵 [List] 버튼 클릭 시 목록 페이지로 이동
+  delBtn.addEventListener('click',()=>{
+      location.href="/qna/remove?bno="+bnoValue;
+  });
+
+  // [List] 버튼 클릭 시 목록 페이지로 이동
   listBtn.addEventListener('click', () => {
+  console.log("asdf");
     location.href = "/qna/list";
   });
 
-  // 🟢 파일 선택 시 이미지 미리보기 및 파일명 출력
+  // 파일 선택 시 이미지 미리보기 및 파일명 출력
   if (uploadInput) {
     uploadInput.addEventListener('change', function () {
       const files = Array.from(this.files);
@@ -80,4 +110,23 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   }
+
 });
+
+
+
+
+async function fileRemoveToServer(uuid) {
+    try {
+        const url = `/qna/customeriqfile/${uuid}`;
+        const config = {
+            method:'delete',
+
+        }
+        const resp = await fetch(url, config);
+        const result = await resp.text();
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
