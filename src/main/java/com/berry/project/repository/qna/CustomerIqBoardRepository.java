1c66b3cc99f2fa857a0e169613f3abfa68c2cb24
rhdqngo99 <rhdqngo99@naver.com>
2025-08-06 09:07:46 +0900

    re:4

--- src/main/java/com/berry/project/repository/qna/CustomerIqBoardRepository.java
+++ src/main/java/com/berry/project/repository/qna/CustomerIqBoardRepository.java
@@ -3,6 +3,5 @@ package com.berry.project.repository.qna;
 import com.berry.project.entity.qna.CustomerIqBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface CustomerIqBoardRepository extends JpaRepository<CustomerIqBoard, Long>, BoardCustomerIqRepository{
 }
